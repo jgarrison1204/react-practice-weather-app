0d26553d2d645e2f8b1a290d0240472e8a311067
jgarrison1204 <jgarrison1204@gmail.com>
2017-05-26 13:55:36 -0700

    State intialized and Input is rendering with handlers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
+import InputWeather from './components/InputWeather';
 import './styles.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userinput: ''
+        }
+    this.handleChange = this.handleChange.bind(this); 
+    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            userinput: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        this.setState({
+            userinput: ''
+        })
+    }
+
     render() {
         return (
             <div className="App">
                 <div className="App-header">
-                  <h2>Welcome to React</h2>
+                  <h2>Enter a City and state</h2>
                 </div>
-                <p className="App-intro">
-                  To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <InputWeather
+                    cityWeather = {this.state.userinput}
+                    handleChange = {this.handleChange}
+                />
             </div>
         );
     }
 }
 
 export default App;
+
