410b99d0cff1e1e35838662646906f06fbc80a17
jgarrison1204 <jgarrison1204@gmail.com>
2017-05-26 17:39:31 -0700

    Added handler in component to prevent default on submit

--- src/components/InputWeather.js
+++ src/components/InputWeather.js
@@ -1,22 +1,35 @@
 import React, {Component} from 'react';
 
-function InputWeather(props) {
-	return (
-		<div>
-			<form onSubmit={props.handleSubmit}>
-				<input
-					placeholder='Los Angeles, California' 
-					type='text'
-					value={props.cityWeather}
-					onChange={props.handleChange}/>
-				<button
-					type='submit'
-					disabled={!props.cityWeather}
-				> Get Weather
-				</button>
-			</form>			
-		</div>
-	)
+class InputWeather extends Component {
+	constructor(props) {
+		super(props);
+
+		this.handleSubmitPreventDefault = this.handleSubmitPreventDefault.bind(this);
+	}
+
+	handleSubmitPreventDefault(event) {
+		event.preventDefault();
+		this.props.handleSubmit(event.target.value);
+	}
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmitPreventDefault}>
+					<input
+						placeholder='Los Angeles, California' 
+						type='text'
+						value={this.props.cityWeather}
+						onChange={this.props.handleChange}/>
+					<button
+						type='submit'
+						disabled={!this.props.cityWeather}
+					> Get Weather
+					</button>
+				</form>			
+			</div>
+		)
+	}
 }
 
 export default InputWeather;
