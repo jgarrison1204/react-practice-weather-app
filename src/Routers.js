81fb8cbed3f54f09a507de9f06265602aa573a25
jgarrison1204 <jgarrison1204@gmail.com>
2017-06-07 19:36:01 -0700

    Routes intialized

---
+++ src/Routers.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch
+} from 'react-router-dom';
+import App from './App';
+import WeatherImage from './components/WeatherImage';
+
+const PlaceHolder = () => {
+	return <p>PlaceHolder</p>
+}
+
+class Navigation extends Component {
+	render() {
+		return (
+			<Router>
+				<Switch>
+					<Route exact path='/' component={App}></Route>
+					<Route exact path='/weather' component={PlaceHolder}></Route>
+				</Switch>
+			</Router>
+		)
+	}
+}
+
+export default Navigation
