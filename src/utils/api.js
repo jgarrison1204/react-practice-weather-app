d8a0ece593910c58d2394b03974858f367250f98
jgarrison1204 <jgarrison1204@gmail.com>
2017-06-15 13:50:13 -0700

    Added multiple calls

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,40 @@
 import Axios from 'axios';
-const apiKey = '63657c7a59c6a9ed5a8e870e4c69a754'
+const weatherApi = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=';
+const apiKey = '&cnt=5&units=imperial&APPID=63657c7a59c6a9ed5a8e870e4c69a754';
 
-function getWeatherDescription(weatherdata) {
-    return weatherdata.map(data => {
-        return data.weather[0].icon;
-    });
+function getWeatherDescription(city) {
+	let getLink = weatherApi + city + apiKey;
+	return Axios.get(getLink)
+		.then(response => {
+		return response.data.list.map(data => {
+	        return data.weather[0].icon;
+	    });
+	})
+}
+
+function getCityNameFromApi(city) {
+	let getLink = weatherApi + city + apiKey;
+	return Axios.get(getLink)
+	.then(response => {
+		return response.data.city.name;
+	})
 }
 
 const apiCall = {
 	fetchWeatherData(city) {	
-	const fetchWeatherData = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=5&units=imperial&APPID=${apiKey}`;
-		return Axios.get(fetchWeatherData)
-			.then(response => {
-				return getWeatherDescription(response.data.list);
-			})
-			.catch(error => {
-				console.log(error)
-			});
+		return Axios.all([
+			getWeatherDescription(city),
+			getCityNameFromApi(city)
+		])
+		.then(response => {
+			 return {
+			 	weatherData: response[0],
+			 	cityName: response[1]
+			 }
+		})
+		.catch(error => {
+			console.log(error)
+		});
 	}
 }
 
