034f648df53600e64f4191d7389850dfaf2ce0e7
jgarrison1204 <jgarrison1204@gmail.com>
2017-06-21 14:12:29 -0700

    Date and image render

--- src/components/WeatherImage.js
+++ src/components/WeatherImage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 
 function WeatherImage(props) {
 	let d = new Date();
@@ -6,8 +7,9 @@ function WeatherImage(props) {
 	let incrementDate = d.setDate(d.getDate() + props.dateIncrementor);
 	let convertFromEpochTime = new Date(incrementDate).toLocaleDateString();
 	return (
-		<div className='col-3 image-container'>
-			<img className='image-wrapper' src={require(`../weather-icons/${props.icon}.svg`)} alt="weather icon"/>
+       // webpack you need to use require(..path) to have images render from the server in localhost.
+		<div className='col-3 image-container' onClick={props.handleRoute}>
+			<img className='image-wrapper' src={require(`../weather-icons/${props.data.icon}.svg`)} alt="weather icon"/>
 			<p className='date-wrapper'>{convertFromEpochTime}</p>
 		</div>
 	)
