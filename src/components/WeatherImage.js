43730a037e54dd3d3b342df19107f62f0582e9e1
jgarrison1204 <jgarrison1204@gmail.com>
2017-06-21 16:50:44 -0700

    Refactored date calculation to be more readable and cleaned up naming conventions

--- src/components/WeatherImage.js
+++ src/components/WeatherImage.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 function WeatherImage(props) {
-	let d = new Date();
-	// getDate returns the day of the month, increment it by passing index from props then setDate returns epoch time.
-	let incrementDate = d.setDate(d.getDate() + props.dateIncrementor);
-	let convertFromEpochTime = new Date(incrementDate).toLocaleDateString();
+	let date = props.data.date;
+	let d = new Date(date * 1000);
+	let formatedDate = d.toLocaleDateString();
 	return (
        // webpack you need to use require(..path) to have images render from the server in localhost.
-		<div className='col-3 image-container' onClick={props.handleRoute}>
+		<div className='image-container' onClick={props.handleRoute}>
 			<img className='image-wrapper' src={require(`../weather-icons/${props.data.icon}.svg`)} alt="weather icon"/>
-			<p className='date-wrapper'>{convertFromEpochTime}</p>
+			<p className='date-wrapper'>{formatedDate}</p>
 		</div>
 	)
 }
