32c07c29858758ad5d4b3566dc95f97814c283f0
jgarrison1204 <jgarrison1204@gmail.com>
2017-06-24 12:32:53 -0700

    Import date converter component

--- src/components/WeatherImage.js
+++ src/components/WeatherImage.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import DateConverter from './DateConverter';
 
 function WeatherImage(props) {
 	let date = props.data.date;
 	let d = new Date(date * 1000);
-	let formatedDate = d.toLocaleDateString();
+	let formatedDate = DateConverter(d.getDay(), d.getMonth(), d.getDate());
 	return (
        // webpack you need to use require(..path) to have images render from the server in localhost.
+		<section className='weather-wrapper'>
 		<div className='image-container' onClick={props.handleRoute}>
 			<img className='image-wrapper' src={require(`../weather-icons/${props.data.icon}.svg`)} alt="weather icon"/>
 			<p className='date-wrapper'>{formatedDate}</p>
 		</div>
+		</section>
 	)
 }
 
