b84d79364d06d569d1e46de8a40bc36bf3b392d8
jgarrison1204 <jgarrison1204@gmail.com>
2017-06-24 12:31:11 -0700

    Added flexbox styling and moved some class around

--- src/App.js
+++ src/App.js
@@ -30,22 +30,29 @@ class App extends Component {
         // variable to hold userinput to be passed as the value to the search key in the to object in the Link component
         let userInputToPassToSearch = this.state.userinput;
         return (
-            <div className="App">
-                <div className="App-header">
-                  <h2>Enter a City and state</h2>
+            <div className='App'>
+                <header className="App-header">
+                        <h2>Enter a City and state</h2>
+                </header>
+                <div className="background-image-wrapper">
+                    <section className="container d-flex justify-content-center align-items-center flex-column page-wrapper">
+                        <InputWeather
+                            cityWeather= {this.state.userinput}
+                            handleChange= {this.handleChange}
+                            handleSubmit= {this.handleSubmit}
+                        />
+                        <Link
+                            style={{
+                                'marginTop':'2%'
+                            }}
+                            className='btn btn-primary'
+                            to={{
+                                pathname: '/weather',
+                                search: `city=${userInputToPassToSearch}`}}>
+                            Click This Button
+                        </Link>
+                    </section>
                 </div>
-                <InputWeather
-                    cityWeather= {this.state.userinput}
-                    handleChange= {this.handleChange}
-                    handleSubmit= {this.handleSubmit}
-                />
-                <Link
-                    className='btn btn-success'
-                    to={{
-                        pathname: '/weather',
-                        search: `city=${userInputToPassToSearch}`}}>
-                    Click This Button
-                </Link>
             </div>
         );
     }
